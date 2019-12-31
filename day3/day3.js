b4e8cc806a6980848f981c991219ed17c6edf8d2
haphamo <hapham0392@gmail.com>
2019-12-31 11:35:53 -0500

    Can't figure out why the coords returned are all the same.

--- day3/day3.js
+++ day3/day3.js
@@ -4,71 +4,55 @@ const wire2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
 const testwire1 = ['R8','U5','L5','D3']
 const testwire2 = ['U7','R6','D4','L4']
 
-let coords = {x: 0, y: 0}
+// let coords = {x: 0, y: 0}
 let intersectingCoords = []
+let testArr = []
+let testArr2 = []
 
+// Function that handles one coord change
 const wireCoords = function(distance){
   amount = parseInt(distance.slice(1))
+  let coords = {x: 0, y: 0}
   switch (distance[0]) {
     case 'R':
       for (let step = 0; step < (amount); step++){
-        coords.x += 1 
-        console.log(coords)
+        coords.x = coords.x + 1
+        
+        testArr.push(coords)
+        console.log(testArr)
       }
-      break
+      
     case 'L':
       for (let step = 0; step > (-amount ); step--){
         coords.x -= 1 
-        console.log(coords)
+        
       }
-      break
     case 'U':
       for (let step = 0; step < (amount); step++){
         coords.y += 1 
-        console.log(coords)
+      //  testArr.push(coords)
       }
-      break
     case 'D':
       for (let step = 0; step > (-amount); step--){
         coords.y -= 1 
-        console.log(coords)
+      //  testArr.push(coords)
       }
-      break
   }
-  // } else if (distance[0] === 'U'){
-  //   for (let step = coords.y; step < (amount + initial); step++){
-  //     coords.y += 1 
-  //     console.log(coords)
-  //   }
-  // } else if(distance[0] === 'L'){
-  //   for (let step = coords.x; step > (-amount); step--){
-  //     coords.x -= 1 
-  //     console.log(coords)
-  //   }
-  // } else if (distance[0] === 'D'){
-  //   for (let step = coords.y; step > (-amount); step--){
-  //     coords.y -= 1 
-  //     console.log(coords)
-  //   }
-  // }
-  
 }
-// wireCoords('D2')
 
-const wireCoordsArr = function(arr) {
-  arr.forEach(move => {
+wireCoords('R3')
+// Function that handles two arrays of coord changes, stringify to compare changes
+const wireCoordsArr = function(wire1) {
+  wire1.forEach(move => {
     wireCoords(move)
+    
   });
+  console.log(testArr)
+  // wire2.forEach(move => {
+  //   wireCoords(move)
+  // });
 }
+// wireCoordsArr(testwire1)
 
-wireCoordsArr(testwire2)
-// Start (0,0)
-// R8: (8, 0), (0, 7)
-// U5: (8, 5), (6, 7)
-// L5: (3, 5), (6, 3)
-// D3: (3, 2), (3, 3)
-
-// R8 (+8, 0), U5 (0, +5), L5 (-5, 0), D3 (0, -3)
-// U7 (0, +7), R6 (+6, 0), D4 (0, -4), L5 (-3, 0)
 
 

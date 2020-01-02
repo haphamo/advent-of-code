f46e369bbb6d79a45fe4869b9e517fe4591e5282
haphamo <hapham0392@gmail.com>
2020-01-01 23:24:23 -0500

    updated function to return 6 random values in a given array

--- day4/day4.js
+++ day4/day4.js
@@ -5,12 +5,19 @@ const puzzleInput = 109165-576723
 // Two adjacent digits are the same (like 22 in 122345).
 // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679)
 
-//create a function that returns a number in a given array
+// create a function that returns a number in a given array
+// return 6 random values in array instead of 1
+
+let result = []
 const numberInArray = function(arr) {
-  //a random number which is the index from 0 - arr.length
-  const index = Math.floor(Math.random() * arr.length)
-  console.log(index)
-  return arr[index]
+  for (let i = 0; i < 6; i++){
+    let index = Math.floor(Math.random() * arr.length)
+    result.push(arr[index])
+    // console.log(result)
+  }
+  // console.log(index)
+  return result
 }
 
-console.log(numberInArray([1,2,3,4,5,6]))
+// console.log(numberInArray([1,2,3,4,5,6]))
+console.log(numberInArray([6,9,9,0,2,3,1]))
